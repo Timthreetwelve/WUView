3d2e778e49f1106bc8795cb4b5b9d31926303a73
Tim Kennedy <KennedyTim@outlook.com>
2022-09-12 13:07:03 -0500

    Update GenBuildInfo to latest

--- WUView/PowerShell/GenBuildInfo.ps1
+++ WUView/PowerShell/GenBuildInfo.ps1
@@ -1,21 +1,31 @@
-﻿param($outputFile="BuildInfo.cs")
+﻿Param(
+    [Parameter(Mandatory = $true)] [string] $assemblyName,
+    [Parameter(Mandatory = $true)] [string] $assemblyVersion,
+    [Parameter(Mandatory = $false)] [string] $outputFile="BuildInfo.cs"
+)
+Write-Host "GenBuildInfo: Assembly name parameter: $assemblyName"
+Write-Host "GenBuildInfo: Assembly version parameter: $assemblyVersion"
 
 $nowUTC = (Get-Date).ToUniversalTime()
-$gitOutput = git rev-parse --short HEAD
-if ($gitOutput.Length -lt 1 ) {
-    $gitOutput = "n/a"
+
+$commitID = git rev-parse --short HEAD
+if ($commitID.Length -lt 1 ) {
+    $commitID = "n/a"
 }
+
 $class =
 "// Copyright(c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 //
 // This file is generated during the pre-build event by GenBuildInfo.ps1.
 // Any edits to this file will be overwritten during the next build!
 
-namespace WUView
+using System;
+
+namespace $assemblyName
 {
     public static class BuildInfo
     {
-        public const string CommitIDString = `"$gitOutput`";
+        public const string CommitIDString = `"$commitID`";
 
         public const string BuildDateString = `"$nowUTC`";
 
@@ -26,4 +36,4 @@ namespace WUView
 Set-Content -Path $outputFile -Value $class
 
 $fullName = Get-Item $outputFile
-Write-Host "GenBuildInfo completed. Output written to $fullName"
+Write-Host "GenBuildInfo: Output written to $fullName"
