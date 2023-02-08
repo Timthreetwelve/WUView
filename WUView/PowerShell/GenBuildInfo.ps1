48f8c2be6021e10c700803009d5d959821b4806a
Tim Kennedy <KennedyTim@outlook.com>
2023-02-08 15:27:42 -0600

    Implemented GitVersion.MsBuild

--- WUView/PowerShell/GenBuildInfo.ps1
+++ WUView/PowerShell/GenBuildInfo.ps1
@@ -3,8 +3,6 @@
     [Parameter(Mandatory = $true)] [string] $assemblyVersion,
     [Parameter(Mandatory = $false)] [string] $outputFile="BuildInfo.cs"
 )
-Write-Host "GenBuildInfo: Assembly name parameter: $assemblyName"
-Write-Host "GenBuildInfo: Assembly version parameter: $assemblyVersion"
 
 $nowUTC = (Get-Date).ToUniversalTime()
 
@@ -13,13 +11,16 @@ if ($commitID.Length -lt 1 ) {
     $commitID = "n/a"
 }
 
+$commitIDFull = git rev-parse HEAD
+if ($commitIDFull.Length -lt 1 ) {
+    $commitIDFull = "n/a"
+}
+
 $class =
 "// Copyright(c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 //
 // This file is generated during the pre-build event by GenBuildInfo.ps1.
 // Any edits to this file will be overwritten during the next build!
-
-using System;
 
 namespace $assemblyName
 {
@@ -27,9 +28,15 @@ namespace $assemblyName
     {
         public const string CommitIDString = `"$commitID`";
 
+        public const string CommitIDFullString = `"$commitIDFull`";
+
+        public const string VersionString = `"$assemblyVersion`";
+
         public const string BuildDateString = `"$nowUTC`";
 
         public static readonly DateTime BuildDateUtc = DateTime.SpecifyKind(DateTime.Parse(BuildDateString), DateTimeKind.Utc);
+
+        public static readonly DateTime BuildDateLocal = BuildDateUtc.ToLocalTime();
     }
 }"
 
