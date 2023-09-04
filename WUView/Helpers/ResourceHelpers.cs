3a36a8fa401c75708c5a22126a99876ff4f525d2
Tim Kennedy <kennedytim@outlook.com>
2023-09-04 17:07:48 -0500

    Initial Resource Dictionary files for language strings

---
+++ WUView/Helpers/ResourceHelpers.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
+
+namespace WUView.Helpers;
+
+internal static class ResourceHelpers
+{
+    #region NLog Instance
+    private static readonly Logger _log = LogManager.GetLogger("logTemp");
+    #endregion NLog Instance
+
+    /// <summary>
+    /// Gets the string resource for the key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns>String</returns>
+    public static string GetStringResource(string key)
+    {
+        try
+        {
+            return Application.Current.TryFindResource(key).ToString();
+        }
+        catch (Exception ex)
+        {
+            _log.Error(ex, $"Resource not found: {key}");
+            return $"Resource not found: {key}";
+        }
+    }
+}
