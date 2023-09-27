0310b20309fb9a53398271244518f846eb9755d8
Tim Kennedy <KennedyTim@outlook.com>
2023-09-27 15:24:11 -0500

    Change UseOSLanguage default value to true. Closes #39.

--- WUView/Configuration/UserSettings.cs
+++ WUView/Configuration/UserSettings.cs
@@ -73,7 +73,7 @@ public partial class UserSettings : ConfigManager<UserSettings>
     private ThemeType _uITheme = ThemeType.System;
 
     [ObservableProperty]
-    private bool _useOSLanguage;
+    private bool _useOSLanguage = true;
 
     [ObservableProperty]
     private double _windowHeight = 650;
