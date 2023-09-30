0af3f9f2c09a4d57cac09b826882fa82af737eb2
Tim Kennedy <KennedyTim@outlook.com>
2023-09-30 16:46:41 -0500

    Updated comments

--- WUView/Helpers/ResultCodeHelper.cs
+++ WUView/Helpers/ResultCodeHelper.cs
@@ -1,8 +1,16 @@
 ﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 
 namespace WUView.Helpers;
+/// <summary>
+/// Class to localize Result Codes.
+/// </summary>
 internal static class ResultCodeHelper
 {
+    /// <summary>
+    /// Localizes the Result Code. Also removes the unwanted "orc" prefix.
+    /// </summary>
+    /// <param name="resultCode">Result code from update history.</param>
+    /// <returns>Result code as localized string.</returns>
     public static string TranslateResultCode(OperationResultCode resultCode)
     {
         switch (resultCode)
