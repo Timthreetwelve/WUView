eb53c75190b04a61ea2bb776c6174b37ccc21c15
Tim Kennedy <timthreetwelve@outlook.com>
2023-12-16 13:56:26 -0600

    Add Slovenian to list of defined languages. #69

--- WUView/Models/UILanguage.cs
+++ WUView/Models/UILanguage.cs
@@ -45,16 +45,17 @@ internal partial class UILanguage : ObservableObject
     /// </summary>
     private static List<UILanguage> LanguageList { get; } = new()
     {
-        new UILanguage {Language = "English", LanguageCode = "en-US", LanguageNative = "English",    Contributor = "Timthreetwelve", Note="Default"},
-        new UILanguage {Language = "English", LanguageCode = "en-GB", LanguageNative = "English",    Contributor = "Timthreetwelve"},
-        new UILanguage {Language = "Spanish", LanguageCode = "es-ES", LanguageNative = "Español",    Contributor = "My AWESOME brother Steve"},
-        new UILanguage {Language = "Italian", LanguageCode = "it-IT", LanguageNative = "Italiano",   Contributor = "RB"},
-        new UILanguage {Language = "Dutch",   LanguageCode = "nl-NL", LanguageNative = "Nederlands", Contributor = "Tim"},
-        new UILanguage {Language = "German",  LanguageCode = "de-DE", LanguageNative = "Deutsch",    Contributor = "Timthreetwelve & Henry2o1o"},
-        new UILanguage {Language = "French",  LanguageCode = "fr-FR", LanguageNative = "Français",   Contributor = "Timthreetwelve"},
-        new UILanguage {Language = "Catalan", LanguageCode = "ca-ES", LanguageNative = "Català",     Contributor = "Timthreetwelve"},
-        new UILanguage {Language = "Polish",  LanguageCode = "pl-PL", LanguageNative = "Polski",     Contributor = "FadeMind"},
-        new UILanguage {Language = "Slovak",  LanguageCode = "sk-SK", LanguageNative = "Slovenčina", Contributor = "VAIO"},
+        new UILanguage {Language = "English",   LanguageCode = "en-US", LanguageNative = "English",     Contributor = "Timthreetwelve", Note="Default"},
+        new UILanguage {Language = "English",   LanguageCode = "en-GB", LanguageNative = "English",     Contributor = "Timthreetwelve"},
+        new UILanguage {Language = "Spanish",   LanguageCode = "es-ES", LanguageNative = "Español",     Contributor = "My AWESOME brother Steve"},
+        new UILanguage {Language = "Italian",   LanguageCode = "it-IT", LanguageNative = "Italiano",    Contributor = "RB"},
+        new UILanguage {Language = "Dutch",     LanguageCode = "nl-NL", LanguageNative = "Nederlands",  Contributor = "Tim"},
+        new UILanguage {Language = "German",    LanguageCode = "de-DE", LanguageNative = "Deutsch",     Contributor = "Timthreetwelve & Henry2o1o"},
+        new UILanguage {Language = "French",    LanguageCode = "fr-FR", LanguageNative = "Français",    Contributor = "Timthreetwelve"},
+        new UILanguage {Language = "Catalan",   LanguageCode = "ca-ES", LanguageNative = "Català",      Contributor = "Timthreetwelve"},
+        new UILanguage {Language = "Polish",    LanguageCode = "pl-PL", LanguageNative = "Polski",      Contributor = "FadeMind"},
+        new UILanguage {Language = "Slovak",    LanguageCode = "sk-SK", LanguageNative = "Slovenčina",  Contributor = "VAIO"},
+        new UILanguage {Language = "Slovenian", LanguageCode = "sl-SL", LanguageNative = "Slovenščina", Contributor = "Jadran Rudec"},
     };
 
     /// <summary>
