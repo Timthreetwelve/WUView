7e1b60d30ec3d85fa81870597a2d9e619ccfed60
Tim Kennedy <timthreetwelve@outlook.com>
2024-03-28 18:25:32 -0500

    Migrated to Vanara PInvoke package for clipboard

--- WUView/Helpers/ClipboardHelper.cs
+++ WUView/Helpers/ClipboardHelper.cs
@@ -1,34 +1,31 @@
 ﻿// Copyright (c) Tim Kennedy. All Rights Reserved. Licensed under the MIT License.
 
-// Based on https://stackoverflow.com/a/30165665
+using static Vanara.PInvoke.User32;
 
 namespace WUView.Helpers;
 
 internal static class ClipboardHelper
 {
-    [DllImport("user32.dll")]
-    private static extern bool OpenClipboard(IntPtr hWndNewOwner);
+    #region Copy text to clipboard
+    private const uint _const_CF_UNICODETEXT = 13;
 
-    [DllImport("user32.dll")]
-    private static extern bool CloseClipboard();
-
-    [DllImport("user32.dll")]
-    private static extern bool SetClipboardData(uint uFormat, IntPtr data);
-
-    private const uint CF_UNICODETEXT = 13;
-
+    /// <summary>
+    /// Copies text to clipboard using Vanara PInvoke instead of DllImport
+    /// </summary>
+    /// <param name="text">Text to be placed in the Windows clipboard</param>
     public static bool CopyTextToClipboard(string text)
     {
-        if (!OpenClipboard(IntPtr.Zero))
+        if (!OpenClipboard(IntPtr.Zero) || text.Length < 1)
         {
             return false;
         }
 
         IntPtr global = Marshal.StringToHGlobalUni(text);
 
-        SetClipboardData(CF_UNICODETEXT, global);
-        CloseClipboard();
+        _ = SetClipboardData(_const_CF_UNICODETEXT, global);
+        _ = CloseClipboard();
 
         return true;
     }
+    #endregion Copy text to clipboard
 }
